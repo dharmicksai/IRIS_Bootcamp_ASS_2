b4adb3620c39242aec55c90b3ea877345f22c8ee
dharmicksai <dharmicksaik@gmail.com>
2020-04-27 12:38:28 +0530

    Added Comments

--- lib/loader.dart
+++ lib/loader.dart
@@ -3,56 +3,56 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 
-class loader extends StatefulWidget {
+class loader extends StatefulWidget { //animated widget
   @override
   _loaderState createState() => _loaderState();
 }
 
-class _loaderState extends State<loader> with SingleTickerProviderStateMixin{
-  AnimationController controller;
-  Animation<double> animation_rotation;
-  Animation<double> animation_radius_in;
-  Animation<double> animation_radius_out;
-  double initialradius = 30 ;
-  double radius=0.0;
+class _loaderState extends State<loader> with SingleTickerProviderStateMixin{ // provides data for every frame
+  AnimationController controller; //controller for animation
+  Animation<double> animation_rotation; //animation for rotation
+  Animation<double> animation_radius_in; //animation for radius in
+  Animation<double> animation_radius_out; //animation for radius out
+  double initialradius = 30 ; //initial radius
+  double radius=0.0; //radius druing execution
 
   @override
-  void initState(){
+  void initState(){ //initialisation of animations
     super.initState();
-    Timer(Duration(seconds: 7), (){
-      Navigator.pop(context);
+    Timer(Duration(seconds: 7), (){  //setting time of animation to 5 seconds
+      Navigator.pop(context); //after end of animations movig to actual home page
       Navigator.pushNamed(context, '/home');} );
-    controller=AnimationController(vsync: this,duration: Duration(seconds: 5));
+    controller=AnimationController(vsync: this,duration: Duration(seconds: 5)); //initialising controller
 
     animation_rotation = Tween<double>(
-      begin: 0.0,
-      end: -1.0,
-    ).animate(CurvedAnimation(parent: controller, curve: Interval(0.0,1.0,curve: Curves.linear)));
+      begin: 0.0, // start of value of rotation
+      end: -1.0, //end of value of rotation
+    ).animate(CurvedAnimation(parent: controller, curve: Interval(0.0,1.0,curve: Curves.linear)));// linear transition between values and during start to end time
 
 
     animation_radius_in = Tween<double>(
-      begin: 1.0,
-      end:0.0,
+      begin: 1.0, //start value of radius
+      end:0.0, //end value of radius
 
-    ).animate(CurvedAnimation(parent: controller, curve: Interval(0.75, 1,curve: Curves.elasticIn)));
+    ).animate(CurvedAnimation(parent: controller, curve: Interval(0.75, 1,curve: Curves.elasticIn)));//elasticin transition between values and during last quarter of time
     animation_radius_out = Tween<double>(
-      begin: 0.0,
-      end:1.0,
+      begin: 0.0,//start value of radius
+      end:1.0,//end value of radius
 
-    ).animate(CurvedAnimation(parent: controller, curve: Interval(0.0, 0.25,curve: Curves.elasticOut)));
-    controller.addListener((){
+    ).animate(CurvedAnimation(parent: controller, curve: Interval(0.0, 0.25,curve: Curves.elasticOut)));//elasticout transition between values and during first quarter of time
+    controller.addListener((){ //changes value of radius
       setState(() {
-        if(controller.value>=0.75&&controller.value<=1.0)
+        if(controller.value>=0.75&&controller.value<=1.0) //if last quarter of rotation
         {
-          radius=initialradius*animation_radius_in.value;
-        }else if(controller.value>=0.0&&controller.value<=0.25)
+          radius=initialradius*animation_radius_in.value; //use radius in
+        }else if(controller.value>=0.0&&controller.value<=0.25) //if first quarter of rotation
         {
-          radius=initialradius*animation_radius_out.value;
+          radius=initialradius*animation_radius_out.value; //use radius out
         }
       });
 
     });
-    controller.repeat();
+    controller.repeat(); //repeats the actions of controller
 
   }
 
@@ -62,17 +62,17 @@ class _loaderState extends State<loader> with SingleTickerProviderStateMixin{
       width: 100.0,
       height: 100.0,
       child: Center(
-        child: RotationTransition(
-          turns: animation_rotation,
-          child: Stack(
+        child: RotationTransition( //Widget which rotates
+          turns: animation_rotation, //animation rotation controlls rotation of this widget
+          child: Stack( //stack widgets one above the other
             children: <Widget>[
-              Dot(
+              Dot( //center Dot
                 radius:20.0,
                 color: Colors.white,
               ),
-              Transform.translate(
+              Transform.translate(//sets position of smaller dots
                 offset: Offset(radius*cos(pi/4), radius*sin(pi/4)),
-                child: Dot(
+                child: Dot( //dot object
                   radius: 10.0,
                   color: Colors.black12,
                 ),
@@ -146,7 +146,7 @@ class Dot extends StatelessWidget {
       child: Container(
         width: this.radius,
         height: this.radius,
-        decoration: BoxDecoration(
+        decoration: BoxDecoration(//provides circle shape to container
           color: this.color,
           shape: BoxShape.circle,
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,23 @@ import 'loader.dart';
 import 'strings.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:async';
-void main() => runApp(MyApp());
+void main() => runApp(MyApp()); //runs My App
 
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage() ,
-      routes: {
-    "/home": (BuildContext context) => HomeScreen(),
+      home: Homepage() ,//sets home page to splash screen
+      routes: {  //giving Navigator routes
+       "/home": (BuildContext context) => HomeScreen(),
     },
 
     );
 
   }
 }
-class Homepage extends StatefulWidget {
+class Homepage extends StatefulWidget { //splash screen sorry for not nameing properly
   @override
   _HomepageState createState() => _HomepageState();
 }
@@ -28,35 +28,35 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [Colors.lightGreenAccent,Colors.blue],
-              begin: Alignment.topLeft,
-              end:Alignment.bottomRight
+
+      body: Container( //sending Contanier for body
+        decoration: BoxDecoration(    //decorations
+          gradient: LinearGradient(   //seting Background colour using gradient of colors
+              colors: [Colors.lightGreenAccent,Colors.blue],  //set staring colour to green and end color to blue
+              begin: Alignment.topLeft, //starting point is top left corner of screen
+              end:Alignment.bottomRight//ending point is bottom right corner
           )
         ),
-        child: Center(
+        child: Center( //placing contents in center
 
-            child: SafeArea(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+            child: SafeArea( //making sure widgets dont go to places where they might get over laped
+              child: Column(  //initialising column
+                mainAxisAlignment: MainAxisAlignment.center,  //alignig children to center
                 children: <Widget>[
-                  Text('Hola ',style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
+                  Text('Hola ',style: TextStyle(  // text widget
+                    fontSize: 40.0, //set font size
+                    fontWeight: FontWeight.bold, //set font weight to bold
                   ),),
                   SizedBox(
-                    height: 30.0,
+                    height: 30.0, //sized Box to give space between loader and text
                   ),
-                  loader(),
+                  loader(), //loader is imported from lib/loader.dart see here for more details
                   SizedBox(
-                    height: 30.0,
+                    height: 30.0, //empty box to give space between loader and sized box
                   ),
                   Text('Please Wait Loading',style: TextStyle(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.w300,
+                    fontSize: 15.0, //setting font size
+                    fontWeight: FontWeight.w300, //setting font weight
                   ),),
                 ],
               ),
@@ -66,16 +66,16 @@ class _HomepageState extends State<Homepage> {
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatefulWidget { //actual home page
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  int day = DateTime.now().day;
-  int month = DateTime.now().month;
-  int year = DateTime.now().year;
+  int day = DateTime.now().day; //provides day number
+  int month = DateTime.now().month; //provides month number
+  int year = DateTime.now().year; //provides year number
 
 
 
@@ -83,55 +83,55 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-         title: Text('$day ${months[month]} $year'),
-          centerTitle: true,
+        appBar: AppBar(  //setting app bar
+         title: Text('$day ${months[month]} $year'), //title: day Month_name year
+          centerTitle: true, //title to be in center
         ),
-        body: buildList(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: (){
-            if(curlindex<strings.length)
+        body: buildList(), //list view
+        floatingActionButton: FloatingActionButton(  //floating acttion button
+          child: Icon(Icons.add),  //icon add is given to button
+          onPressed: (){ //providing on press action
+            if(curlindex<strings.length)  // checking if all elemnts are added to screen
               {
-                showDialog(
+                showDialog(  //adding dialog
 
-                    context: context,
-                    builder:(BuildContext context){
-                      return Dialog(
+                    context: context, //context of diaalog
+                    builder:(BuildContext context){  //builder of dialog box
+                      return Dialog( //dialog to ask wether to add data
 
 
-                        backgroundColor: Colors.blue,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
+                        backgroundColor: Colors.blue, //background color
+                        child: Column( //column for widgets
+                          mainAxisSize: MainAxisSize.min, //size of column should be as minimum as possible
                           children: <Widget>[
-                            Text('Hi',style: TextStyle(
-                              fontSize: 20.0
+                            Text('Hi,',style: TextStyle( // text widget
+                              fontSize: 20.0 //font size
                             ),),
                             Text('Do u want to add event:',style:TextStyle(
-                              fontSize: 20.0
+                              fontSize: 20.0 //font size
                             ),),
                             Text(' ${events[strings[curlindex]]}',style: TextStyle(
-                              fontSize: 20
+                              fontSize: 20 //font size
                             ),),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
+                            Row( //row of buttons
+                              mainAxisAlignment: MainAxisAlignment.center, //placeing buttons in center
                               children: <Widget>[
-                                FlatButton(
-                                    color: Colors.lightGreenAccent,
-                                    onPressed: (){
-                                      data.add(strings[curlindex]);
-                                      curlindex++;
-                                      setState(() {});
-                                      Navigator.pop(context);
+                                FlatButton(//add button
+                                    color: Colors.lightGreenAccent,//button color
+                                    onPressed: (){ //on pressed action
+                                      data.add(strings[curlindex]); //adding data from strings
+                                      curlindex++; //incrimenting length of data
+                                      setState(() {}); //implemnting changes
+                                      Navigator.pop(context); //removing dialog
 
                                     },
                                     child: Text('yes',style: TextStyle(
-                                      fontSize: 20.0
+                                      fontSize: 20.0//font size
                                     ),)
                                 ),
-                                FlatButton(
+                                FlatButton(//dont add button
                                   onPressed: (){
-                                    Navigator.pop(context);
+                                    Navigator.pop(context);//remove dialog
                                   },
                                   child:Text('No') ,
                                   color: Colors.red,
@@ -147,7 +147,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     }
                 );
                 setState(() {});
-              } else {
+              } else { //dialog to show end of file
               showDialog(
                   context: context,
                   builder: (BuildContext context) {
@@ -155,10 +155,10 @@ class _HomeScreenState extends State<HomeScreen> {
                       title: Text('Alert'),
                       content: Text('End of list!'),
                       actions: <Widget>[
-                        FlatButton(
+                        FlatButton(//button to remove allert dialog
                           child: Text('OK'),
                           onPressed: () {
-                            Navigator.pop(context);
+                            Navigator.pop(context);//removing dialog
                           },
                         )
                       ],
@@ -169,13 +169,13 @@ class _HomeScreenState extends State<HomeScreen> {
           },
 
         ),
-        backgroundColor: Colors.lightGreenAccent,
+        backgroundColor: Colors.lightGreenAccent,//setting back ground color to light green
       ),
     );
 
   }
 }
-List<String>data = [
+List<String>data = [ //data off app
   '26 Jan',
   '10 Mar',
   '25 Mar',
@@ -190,28 +190,28 @@ List<String>data = [
 int curlindex=6;
 
 Widget buildList() {
-  return ListView.builder(
-    itemCount: data.length,
-    itemBuilder: (BuildContext context, int index) {
+  return ListView.builder( //list view
+    itemCount: data.length, //length of list view
+    itemBuilder: (BuildContext context, int index) { // widget builder
       return Container(
         padding: EdgeInsets.symmetric(vertical:10.0,horizontal:50.0),
-        child: ExpansionTile(
+        child: ExpansionTile( //adding expansion on clicking
           title: Text(data[index]+" : "+ events[data[index]]),
-          children: <Widget>[
+          children: <Widget>[ //children r shown when pressed on widget
             Card(
               child: Padding(
                 padding: const EdgeInsets.symmetric(vertical:10.0,horizontal: 80.0),
-                child:FlatButton(
+                child:FlatButton(//button to launch url
                     onPressed:() async{
-                      if (await canLaunch(url[data[index]])) {
-                        await launch(url[data[index]]);
-                      } else {
+                      if (await canLaunch(url[data[index]])) { //check if it can launch url
+                        await launch(url[data[index]]); //launch url
+                      } else { //else throw exception
                         print('could not launch');
                         throw 'Could not launch url';
                       }
 
                     },
-                    child: Icon(Icons.launch)),
+                    child: Icon(Icons.launch)),//icon for launching
               ),
             ),
           ],
